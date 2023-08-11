cf64918d373e525c4e555338c83bb58189d7c38b
iamagenus <132496206+iamagenus@users.noreply.github.com>
2023-08-11 11:46:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'Non-Essential Employee',
+	name: 'Nameless Helper',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: true,
