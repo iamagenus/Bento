79a7f9b29aca8c846881f4890bbdabf36107908d
iamagenus <132496206+iamagenus@users.noreply.github.com>
2023-08-11 15:52:35 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'Nameless Thankless',
+	name: 'You Nincompoop',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: true,
